f92a31e19020eaf0bd2225eb2c0397d69b43beb6
ankitverma097 <ankitmmb2016@gmail.com>
2021-09-01 13:33:19 +0530

    initial

---
+++ js/first.js
@@ -0,0 +1,12 @@
+//condition
+// let age=17;
+// let hasvoterid = 'yes';
+// if(age >=18 && hasvoterid == 'yes'){
+//     alert("mishra gandu");
+    
+// }else if(age >=18 && hasvoterid == 'no'){
+//     alert("gand marvavo mishra");
+// }
+// else{
+//     alert("mishra not gandu")
+// }
